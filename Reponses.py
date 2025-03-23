efc264559ee229b7c0c115e4115ea90914d494a8
jojodu0747 <napolitanojoe07@gmail.com>
2025-03-23 16:32:34 +0100

    ajout reponses aux questions

---
+++ Reponses.py
@@ -0,0 +1,26 @@
+import panda as pd
+
+# Q1
+
+Q1 = athlete_event[
+    (athlete_event["Name"].str.contains("Michael Fred Phelps, II", case=False))
+].dropna()
+len(Q1)
+
+# Q2
+
+# Q3
+
+# Q4
+
+# Q5
+
+# Q6
+
+# Q7
+
+# Q8
+
+# Q9
+
+# Q10
