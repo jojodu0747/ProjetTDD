12ecb69c1648f36cb7b92363058f98a3fcdcc9b3
jojodu0747 <napolitanojoe07@gmail.com>
2025-03-23 19:20:03 +0100

    ajout de la Q3

--- Reponses.py
+++ Reponses.py
@@ -11,6 +11,21 @@ len(Q1)
 
 # Q3
 
+# création d'une table saut en longueur
+table_saut_longueur = athlete_event[
+    (athlete_event["Sport"].str.contains("Athletics"))
+    & (athlete_event["Season"] == "Summer")
+    & athlete_event["Event"].str.contains(
+        "Athletics Men's Long Jump|Athletics Women's Long Jump"
+    )
+]
+# Grouper par nation et compter le nombre de médailles de chacune d'entre elles
+nombre_medailles = table_saut_longueur.groupby("NOC").size()
+# Trier les résultats par ordre décroissant
+classement = nombre_medailles.sort_values(ascending=False)
+
+classement[0:5]
+
 # Q4
 
 # Q5
