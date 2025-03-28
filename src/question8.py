2d467d95a97003987df172572b931abbd170cdb4
LouisSta <louis.stanisiere@eleve.ensai.fr>
2025-03-28 13:03:15 +0100

    Update question8.py

---
+++ src/question8.py
@@ -0,0 +1,6 @@
+# Identifier les pays qui ont le ratio nombre de médailles
+# gagnées par des hommes sur nombre de médailles gagnées
+# par des femmes le plus haut/le plus bas ?
+
+from base_dd import BDD_EVENTS
+from base_dd
