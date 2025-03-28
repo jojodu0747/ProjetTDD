0881446c058a08f058e1128f183c1468855a0dbe
LouisSta <louis.stanisiere@eleve.ensai.fr>
2025-03-28 13:40:58 +0100

    Fini Q8

--- src/question8.py
+++ src/question8.py
@@ -3,5 +3,19 @@
 # par des hommes le plus haut/le plus bas ?
 
 from base_dd import BDD_EVENTS
-bdd_pays_sexes = BDD_EVENTS.groupby(["NOC", "Sex"]).size()
-print(bdd_pays_sexes)
+#unstack sert à mettre F et M en variables et non en valeurs de Sex:
+bdd_pays_sexes = BDD_EVENTS.groupby(["NOC", "Sex"]).size().unstack(fill_value=0)
+
+if len(bdd_pays_sexes[(bdd_pays_sexes["M"] == 0)]) != 0:
+    print("Dans ces pays, seules des femmes ont gagné des médailles:", bdd_pays_sexes[(bdd_pays_sexes["M"] == 0)])
+    bdd_pays_sexes = bdd_pays_sexes[(bdd_pays_sexes["M"] != 0)]
+
+bdd_pays_sexes["Ratio_F_H"] = bdd_pays_sexes["F"] / bdd_pays_sexes["M"]
+
+bdd_pays_sexes = bdd_pays_sexes.sort_values(by="Ratio_F_H", ascending=False)
+
+print("Ces pays ont le ratio F/H le plus haut : ", bdd_pays_sexes.head(10))
+print("Ces pays ont le ratio F/H le plus bas : ", bdd_pays_sexes.tail(10))
+
+#idee 1 : remplacer les NOC par des pays
+#idee 2 : choisir le nombre medailles (notion de représentativité)
