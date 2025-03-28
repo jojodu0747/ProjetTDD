0a15f66b87c69c04e38a564cff60c15a244592fe
ShiteAdoSimp <aziz.seghaier1@gmail.com>
2025-03-28 12:08:11 +0100

    a

--- Projet_Joé/Projetjoé.py
+++ Projet_Joé/Projetjoé.py
@@ -1,21 +1,21 @@
 import pandas as pd
 import numpy as np
 
-athlete_event = pd.read_csv(
-    r"C:\Users\napol\Documents\Python\projTDD\ProjetTDD\donnees_jeux_olympiques\donnees_jeux_olympiques\athlete_events.csv"
+BDD_EVENTS = pd.read_csv(
+    r"C:\Users\napol\Documents\Python\projTDD\ProjetTDD\donnees_jeux_olympiques\donnees_jeux_olympiques\BDD_EVENTSs.csv"
 )
 
-print(athlete_event.head())  # avoir un résumé des premiere ligne de la bdd
+print(BDD_EVENTS.head())  # avoir un résumé des premiere ligne de la bdd
 
-athlete_event.describe()  # avoir un résumé de la bdd
-print(athlete_event.columns)  # avoir les colonnes de la BDD
+BDD_EVENTS.describe()  # avoir un résumé de la bdd
+print(BDD_EVENTS.columns)  # avoir les colonnes de la BDD
 
 
 # Déterminez le nombre de médailles gagnées par Michael Phelps. Son nom complet est Michael
 # Fred Phelps, II
 
-Q1 = athlete_event[
-    (athlete_event["Name"].str.contains("Michael Fred Phelps, II", case=False))
+Q1 = BDD_EVENTS[
+    (BDD_EVENTS["Name"].str.contains("Michael Fred Phelps, II", case=False))
 ].dropna()
 len(Q1)
 # on conditionne directement sur la colonnne Name avec str.contains (propre à Panda)
@@ -24,10 +24,10 @@ len(Q1)
 # na = False peut être utile si ça renvoie des na dans la requète, .dropna() enleve les na
 
 # création d'une table saut en longueur
-table_saut_longueur = athlete_event[
-    (athlete_event["Sport"].str.contains("Athletics"))
-    & (athlete_event["Season"] == "Summer")
-    & athlete_event["Event"].str.contains(
+table_saut_longueur = BDD_EVENTS[
+    (BDD_EVENTS["Sport"].str.contains("Athletics"))
+    & (BDD_EVENTS["Season"] == "Summer")
+    & BDD_EVENTS["Event"].str.contains(
         "Athletics Men's Long Jump|Athletics Women's Long Jump"
     )
 ]
@@ -40,12 +40,12 @@ nombre_medailles = table_saut_longueur.groupby("NOC").size()
 classement = nombre_medailles.sort_values(ascending=False)
 classement[0:5]
 
-print("Type de 'Sport' :", athlete_event["Sport"].dtype)
-print("Type de 'Season' :", athlete_event["Season"].dtype)
+print("Type de 'Sport' :", BDD_EVENTS["Sport"].dtype)
+print("Type de 'Season' :", BDD_EVENTS["Season"].dtype)
 
-athlete_event["Sport"].unique()
-athlete_event["Season"].unique()
+BDD_EVENTS["Sport"].unique()
+BDD_EVENTS["Season"].unique()
 
-athlete_event[athlete_event["Season"] == "Summer"]
+BDD_EVENTS[BDD_EVENTS["Season"] == "Summer"]
 
-athlete_event["NOC"]
+BDD_EVENTS["NOC"]
