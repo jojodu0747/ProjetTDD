f0ac747de6bd3b27f7c302e77b81c7f119a7e001
LouisSta <louis.stanisiere@eleve.ensai.fr>
2025-03-28 13:16:57 +0100

    jsp

--- src/question8.py
+++ src/question8.py
@@ -1,6 +1,7 @@
 # Identifier les pays qui ont le ratio nombre de médailles
-# gagnées par des hommes sur nombre de médailles gagnées
-# par des femmes le plus haut/le plus bas ?
+# gagnées par des femmes sur nombre de médailles gagnées
+# par des hommes le plus haut/le plus bas ?
 
 from base_dd import BDD_EVENTS
-from base_dd
+bdd_pays_sexes = BDD_EVENTS.groupby(["NOC", "Sex"]).size()
+print(bdd_pays_sexes)
