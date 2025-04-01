be6c361794510a530607bf8bff6f2166b8d81ffe
ShiteAdoSimp <aziz.seghaier1@gmail.com>
2025-04-01 14:23:50 +0200

    a

--- src/question10_py_pur.py
+++ src/question10_py_pur.py
@@ -1,3 +1,7 @@
-from base_dd import adresse
+import os
+
+adresse = (os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+           + "/donnees_jeux_olympiques/")
+
 with open(adresse + 'athlete_events.csv', 'r', newline='') as csvfile:
   print("test")
