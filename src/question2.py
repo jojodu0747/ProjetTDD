fb48bafae9e26392afc429c3d060b590d84b5966
ShiteAdoSimp <aziz.seghaier1@gmail.com>
2025-04-01 03:40:08 +0200

    Update question2.py

--- src/question2.py
+++ src/question2.py
@@ -1,17 +1,16 @@
 from base_dd import BDD_EVENTS
-import matplotlib.pyplot as plt
 
 
 def question2(liste_sessions, type_medaille, combine):
     """Cette fonction répond à la question 2.
     Il donne les bornes inférieur et supérieur du nombre de médailles obtenue par chaque
     nation, selon les types de médailles pris en compte, et selon les sessions pris en
-    compte. Il retourne un graphique dont la nature dépend des paramètres
+    compte.
 
     Parameters
     __________
 
-    liste_sessions: list[str]
+    liste_sessions: list[tuple]
         Liste des sessions prises en compte par la fonction
 
     type_medaille: list[str]
@@ -23,14 +22,26 @@ def question2(liste_sessions, type_medaille, combine):
     Returns
     _______
 
-    list[tuple[int]]
-        La liste des couple (inf, max) pour chaque session ou pour l'ensemble des
-        sessions selon combine.
-        Affiche en parallèle des graphiques.
+    list[tuple[tuple]]
+        La liste des couple ((argmin, min), (argmax, max)) pour chaque session ou pour
+        l'ensemble des sessions selon combine.
     """
-    pass
-
-
-combine = False
-liste_sessions = []
-type_medaille = []
+    df = BDD_EVENTS.loc[
+        BDD_EVENTS['Medal'].isin(type_medaille)
+        & BDD_EVENTS['Games'].isin(liste_sessions),
+        ['ID', 'Team', 'Games', 'Medal']
+        ]
+    if combine:
+        ag = df.groupby('Team')['Medal'].count()
+        min = (list(ag.iloc[[ag.argmin()]].index)[0], int(ag.min()))
+        max = (list(ag.iloc[[ag.argmax()]].index)[0], int(ag.max()))
+        res = [(min, max)]
+    else:
+        res = []
+        for session in liste_sessions:
+            ag = (df.loc[df['Games']
+                         == session].groupby(['Team', 'Games'])['Medal'].count())
+            min = (list(ag.iloc[[ag.argmin()]].index)[0][0], int(ag.min()))
+            max = (list(ag.iloc[[ag.argmax()]].index)[0][0], int(ag.max()))
+            res.append((min, max))
+    return res
