c3db94921a52cbe0bc447fd935d33d400044b41d
LouisSta <louis.stanisiere@eleve.ensai.fr>
2025-04-01 11:32:33 +0200

    Update question8.py

--- src/question8.py
+++ src/question8.py
@@ -2,21 +2,25 @@
 # gagnées par des femmes sur nombre de médailles gagnées
 # par des hommes le plus haut/le plus bas ?
 
-from base_dd import BDD_EVENTS
+from base_dd import BDD_EVENTS, BDD_REGIONS
+BDD_EVENTS_filtre = BDD_EVENTS[BDD_EVENTS["NOC"] != "UNK"]  #supprime pays inconnus
 #unstack sert à mettre F et M en variables et non en valeurs de Sex:
-bdd_pays_sexes = BDD_EVENTS.groupby(["NOC", "Sex"]).size().unstack(fill_value=0)
+bdd_pays_sexes = BDD_EVENTS_filtre.groupby(["NOC", "Sex"]).size().unstack(fill_value=0)
 
 if len(bdd_pays_sexes[(bdd_pays_sexes["M"] == 0)]) != 0:
     print("Dans ces pays, seules des femmes ont gagné des médailles:", bdd_pays_sexes[(bdd_pays_sexes["M"] == 0)])
     bdd_pays_sexes = bdd_pays_sexes[(bdd_pays_sexes["M"] != 0)]
 
-bdd_pays_sexes["Ratio_F_H"] = bdd_pays_sexes["F"] / bdd_pays_sexes["M"]
+bdd_pays_sexes["Ratio_F_H"] = bdd_pays_sexes["F"] / bdd_pays_sexes["M"]  #creation ratio
+bdd_pays_sexes.sort_values(by="Ratio_F_H", ascending=False, inplace=True)
 
-bdd_pays_sexes = bdd_pays_sexes.sort_values(by="Ratio_F_H", ascending=False)
+bdd_pays_sexes = bdd_pays_sexes.merge(BDD_REGIONS[['NOC', 'region', 'notes']], on='NOC', how='left')
+bdd_pays_sexes.rename(columns={'region': 'Pays'}, inplace=True)
+bdd_pays_sexes.drop(columns=['NOC'], inplace=True)
 
 print("Ces pays ont le ratio F/H le plus haut : \n", bdd_pays_sexes.head(10))
 print("Ces pays ont le ratio F/H le plus bas : \n", bdd_pays_sexes.tail(10))
 
 #idee 1 : remplacer les NOC par des pays
-#idee 2 : choisir le nombre medailles (notion de représentativité)
+#idee 2 : choisir le nombre medailles min (notion de représentativité)
 #idee 3 : periode (f(x1,x2,...,periode=None))
