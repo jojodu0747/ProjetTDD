07b86cd5e8a7a18cb66f67b868d61b20f8b9ab45
LouisSta <louis.stanisiere@eleve.ensai.fr>
2025-04-01 13:06:44 +0200

    Delete question10_python_base.py

--- src/question10_python_base.py
+++
@@ -1,5 +0,0 @@
-# Quels sont les sports où la France a gagné le plus de médaille ?
-from base_dd import adresse
-
-with open(adresse + 'athlete_events.csv', 'w', newline='') as csvfile:
-    print("test")
