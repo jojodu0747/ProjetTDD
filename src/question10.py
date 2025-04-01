594009c917108cfd7181facc83bd688f9c37c74e
LouisSta <louis.stanisiere@eleve.ensai.fr>
2025-04-01 12:57:24 +0200

    QBase

--- src/question10.py
+++ src/question10.py
@@ -1,11 +1,10 @@
-# Quel sont les sports où la France a gagné le plus de médaille ?
+# Quels sont les sports où la France a gagné le plus de médaille ?
 from base_dd import BDD_EVENTS
 
 #bdd avec médailles gagnées par la France:
 med_France = BDD_EVENTS[(BDD_EVENTS["NOC"] == "FRA") & (BDD_EVENTS["Medal"].notna())]
 
 #nombre de médailles gagnées par la France par sport
-
 nb_med_France = med_France.groupby(["Sport"]).size()
 nb_med_France_sorted = nb_med_France.sort_values(ascending=False)
 print(nb_med_France_sorted.head(10))
---
+++ src/question10_python_base.py
@@ -0,0 +1,5 @@
+# Quels sont les sports où la France a gagné le plus de médaille ?
+from base_dd import adresse
+
+with open(adresse + 'athlete_events.csv', 'w', newline='') as csvfile:
+    print("test")
