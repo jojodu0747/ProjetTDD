786568ff13bb93f1deec1860489351a49f3ee200
jojodu0747 <napolitanojoe07@gmail.com>
2025-04-01 13:13:44 +0200

    Update question1.py

--- src/question1.py
+++ src/question1.py
@@ -4,3 +4,15 @@ Q1 = BDD_EVENTS[
     (BDD_EVENTS["Name"].str.contains("Michael Fred Phelps, II", case=False))
 ].dropna()
 print(len(Q1))
+
+
+# #en python base
+
+# medal_count = 0
+# for ligne in BDD_EVENTS:
+#     a,b = ligne['Name'], ligne['Medal']
+#     if "Michael Fred Phelps, II" == a and b != 'Na' :
+#         medal_count += 1
+
+# print(medal_count)
+=
