bdd2ec8f7a133e650fcf78e85e61096f96517401
LouisSta <louis.stanisiere@eleve.ensai.fr>
2025-04-09 14:51:02 +0200

    Update question10_py_pur.py

--- src/question10_py_pur.py
+++ src/question10_py_pur.py
@@ -1,7 +1,1 @@
 import os
-
-adresse = (os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-           + "/donnees_jeux_olympiques/")
-
-with open(adresse + 'athlete_events.csv', 'r', newline='') as csvfile:
-  print("test")
