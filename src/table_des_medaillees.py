c0bd9a90a860e1dc6ccb8e16e5d40363209dfa90
jojodu0747 <napolitanojoe07@gmail.com>
2025-04-11 12:44:56 +0200

    qxjiqshi

--- src/table_des_medaillees.py
+++ src/table_des_medaillees.py
@@ -4,4 +4,11 @@ import numpy as np
 
 # création de la fonction qui restreint une table au sport
 
-# affichage des différents sports (dans la doc)
+# affichage des différents sports (dans la doc et peut être utile dans les condtions de succès de la fonction)
+BDD_EVENTS["Sport"].drop_duplicates().tolist()
+
+print(BDD_EVENTS["Sport" == ['Ski Jumping', 'Volleyball']])
+
+def table_par_sport (L):
+    for a in L:
+    BDD_EVENTS["Sport"][f"{a}"]
