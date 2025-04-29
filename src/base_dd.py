51c9d87e5f491aedcc987e009ce570ec303b9c47
ShiteAdoSimp <aziz.seghaier1@gmail.com>
2025-04-29 12:16:43 +0200

    bdd

--- src/base_dd.py
+++ src/base_dd.py
@@ -1,8 +1,41 @@
 import os
 import pandas as pd
+import csv
 
 adresse = (os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
            + "/donnees_jeux_olympiques/")
 
 BDD_EVENTS = pd.read_csv(adresse + "athlete_events.csv")
 BDD_REGIONS = pd.read_csv(adresse + "noc_regions.csv")
+
+BDD_EVENTS.drop_duplicates()
+
+BDD = []
+ID = "1"
+n_doublon = set()
+n_ligne = 2
+header = True
+
+with open(adresse, 'r', newline='') as csvfile:
+    spamreader = csv.reader(csvfile)
+    for row in spamreader:
+        if header is True:
+            header = False
+            continue
+        elif row[0] != ID:
+            BDD = [row]
+            ID = row[0]
+            n_ligne += 1
+            continue
+        elif row in BDD:
+            n_doublon.add(n_ligne)
+            n_ligne += 1
+        else:
+            BDD.append(row)
+            n_ligne += 1
+
+del BDD, ID, n_ligne, header
+
+
+#n_doublon correspond à l'ensemble des lignes qui sont des doublons
+#(1 étant la ligne de l'header)
