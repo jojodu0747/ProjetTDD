2f0a8251b93389287a3286086086a7a7007c099d
LouisSta <louis.stanisiere@eleve.ensai.fr>
2025-05-05 21:27:57 +0200

    retire com

--- src/question10_py_pur.py
+++ src/question10_py_pur.py
@@ -34,12 +34,11 @@ def plus_medailles_pur(region="France", limit=10, offset=0, years=None,
     dic_nb_sport = {}
     with open(adresse + "noc_regions.csv", 'r', newline='') as bdd_regions:
         sprd_reg = csv.reader(bdd_regions)
-        header_reg = next(sprd_reg)  # rend le header et l'"enlève" du spamreader
-        idx_noc_reg = header_reg.index(
-            'NOC')  # indices de ces variables dans noc_regions
+        header_reg = next(sprd_reg)
+        idx_noc_reg = header_reg.index('NOC')
         idx_region = header_reg.index('region')
         for row in sprd_reg:
-            if row[idx_region] == region:  # on trouve le noc correspondant à region
+            if row[idx_region] == region:
                 noc = row[idx_noc_reg]
                 break
         if noc == "":
@@ -48,12 +47,12 @@ def plus_medailles_pur(region="France", limit=10, offset=0, years=None,
     with open(adresse + "athlete_events.csv", 'r', newline='') as bdd_athlete:
         spamreader = csv.reader(bdd_athlete)
         header = next(spamreader)
-        idx_NOC = header.index('NOC')  # indices de ces variables dans athlete_event
+        idx_NOC = header.index('NOC')
         idx_medal = header.index('Medal')
         idx_sport = header.index('Sport')
         idx_year = header.index('Year')
         n_ligne = 2
-        for row in spamreader:  # on parcourt la bdd, row est un individu
+        for row in spamreader:
             if n_ligne in n_doublon:
                 n_ligne += 1
                 continue
