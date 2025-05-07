637b674cb2f8f0816670ae8869334364277be48a
ShiteAdoSimp <aziz.seghaier1@gmail.com>
2025-05-07 23:25:44 +0200

    a

--- src/reglog.py
+++ src/reglog.py
@@ -41,13 +41,12 @@ def modelerl(Sport=None):
     df_1 = df[df["Medal"] == 1]
     df_0 = df[df["Medal"] == 0]
     if df_0.shape[0] > df_1.shape[0]:
-        df_0_sampled = df_0.sample(n=len(df_1), random_state=42)
+        df_0_sampled = df_0.sample(n=len(df_1))
         df_1_sampled = df_1.copy()
     else:
-        df_1_sampled = df_1.sample(n=len(df_0), random_state=42)
+        df_1_sampled = df_1.sample(n=len(df_0))
         df_0_sampled = df_0.copy()
-    df = pd.concat([df_1_sampled, df_0_sampled], axis=0).sample(
-        frac=1, random_state=42).reset_index(drop=True)
+    df = pd.concat([df_1_sampled, df_0_sampled], axis=0)
     X = df.drop(columns='Medal')
     Y = df['Medal']
     Y = np.round(Y).astype(int)
