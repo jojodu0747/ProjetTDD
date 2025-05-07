e4052d2d969168b0a95a7d2871b9397c886b5843
ShiteAdoSimp <aziz.seghaier1@gmail.com>
2025-05-07 17:52:50 +0200

    a

--- src/question6.py
+++
@@ -1,8 +0,0 @@
-from base_dd import BDD_EVENTS
-
-BDD_EVENTS_medalists = BDD_EVENTS[BDD_EVENTS["Medal"].notna()]
-
-# Calculer la moyenne d'âge par sexe
-average_age_by_sex = BDD_EVENTS_medalists.groupby("Sex")["Age"].mean()
-
-print(average_age_by_sex)
